ab9c980f278a89b3eb8eeb3268b285d7697e8483
Michael Easter <codetojoy@gmail.com>
2021-06-20 07:03:17 -0300

    Java example

--- java/Example.java
+++ java/Example.java
@@ -1,27 +1,32 @@
 
 // requires JDK 10+
 public class Example {
-    static void fizz(int x) {
-        System.out.println(x + " fizz");
+    static void fizz() {
+        System.out.println("fizz");
     }
-    static void buzz(int x) {
-        System.out.println(x + " buzz");
+    static void buzz() {
+        System.out.println("buzz");
     }
-    static void fizzbuzz(int x) {
-        System.out.println(x + " fizzbuzz");
+    static void fizzbuzz() {
+        System.out.println("fizzbuzz");
+    }
+    static void emit(int x) {
+        System.out.println(x);
     }
     public static void main(String... args) {
 // ----- common block
 var x = 1;
-while (x <= 30) {
+while (x <= 100) {
     var div3 = (x % 3 == 0);
     var div5 = (x % 5 == 0);
     if (div3 && div5) {
-        fizzbuzz(x);
+        fizzbuzz();
     } else if (div3) {
-        fizz(x);
+        fizz();
     } else if (div5) {
-        buzz(x);
+        buzz();
+    } else {
+        emit(x);
     }
     x += 1;
 }
--- java/run.sh
+++ java/run.sh
@@ -3,4 +3,6 @@
 set -e
 
 javac Example.java
-java -cp . Example 
+java -cp . Example | tee tmp.log
+diff tmp.log ../resources/golden.master.out.log
+echo "diff result: $?"
